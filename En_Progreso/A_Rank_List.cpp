e37f4fda9c712a6c5550408d3f79d0900e685164
Matias Viola <matiasvioladi@gmail.com>
2024-04-19 20:10:09 -0300

    fix: se corrigio A_Rank_List.cpp pero aun no pasa test4

--- En_Progreso/A_Rank_List.cpp
+++ En_Progreso/A_Rank_List.cpp
@@ -5,26 +5,38 @@ using namespace std;
 
 typedef unsigned int u_int;
 
-int main() {
-  u_int n_equipos,k_lugar, resultado = 0;
-  pair<u_int,u_int> par;
+int main()
+{
+  u_int n_equipos, k_lugar, resultado = 1;
+  pair<u_int, u_int> par;
   cin >> n_equipos;
   cin >> k_lugar;
-  vector<pair<u_int,u_int>> primerosK(k_lugar);
+  vector<pair<u_int, u_int>> primerosK(k_lugar);
 
-  for (u_int i = 0; i < k_lugar; i++) {
+  // Llenamos el vector con los primeros k equipos
+  for (u_int i = 0; i < k_lugar; i++)
+  {
     cin >> par.first >> par.second;
     primerosK[i] = par;
   }
-  pair<u_int,u_int> k_equipo = primerosK[k_lugar-1];
-  for (u_int i = k_lugar; i < n_equipos; i++) {
+  pair<u_int, u_int> k_equipo = primerosK[k_lugar - 1];
+
+  // Recorremos los equipos restantes y
+  // contamos cuantas veces aparece el equipo k
+  for (u_int i = k_lugar; i < n_equipos; i++)
+  {
     cin >> par.first >> par.second;
-    if (par == k_equipo) 
+    if (par == k_equipo)
       resultado++;
   }
-  for (u_int i = 0; i < k_lugar - 1; i++) {
-    if (primerosK[i] == k_equipo) 
-          resultado++;
+
+  // Contamos cuantas veces aparece el equipo k en los primeros k lugares
+  for (u_int i = 0; i < k_lugar - 1; i++)
+  {
+    if (primerosK[i] == k_equipo)
+      resultado++;
   }
-  cout << resultado;
+
+  // Imprimimos el resultado
+  cout << resultado << endl;
 }
