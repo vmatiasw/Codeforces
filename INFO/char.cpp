5868917d05c19094edab2ff4a76e46490912a6f9
Matias Viola <90460078+vmatiasw@users.noreply.github.com>
2024-04-21 10:58:49 -0300

    Actualizar INFO con char.cpp

---
+++ INFO/char.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+#define fore(i, a, b) for (ull i = a; i < (b); i++)
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(nullptr);
+
+  char c;
+  cin >> c;
+
+  c = tolower(c);
+  c = toupper(c);
+
+  cout << c << '\n';
+
+
+}
